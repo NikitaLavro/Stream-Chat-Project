00dda7612c34f0c28177be2e12f0711d5fbfbed6
Nikita <lavro.nikita123@gmail.com>
2023-02-02 13:43:01 +0200

    AuthLayout structure

--- client/chat-client/src/pages/layouts/AuthLayout.tsx
+++ client/chat-client/src/pages/layouts/AuthLayout.tsx
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router-dom";
 
+//Components
+import { FullScreenCard } from "../../components/FullScreenCard";
+
 const AuthLayout = () => {
-  return <Outlet />;
+  return (
+    <FullScreenCard>
+      <FullScreenCard.Body>
+        <Outlet />
+      </FullScreenCard.Body>
+      <FullScreenCard.BelowCard>Hi</FullScreenCard.BelowCard>
+    </FullScreenCard>
+  );
 };
 
 export default AuthLayout;
