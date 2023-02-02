90e4d146c414dcb08eb2fdc2f0958badc7173b3d
Nikita <lavro.nikita123@gmail.com>
2023-02-02 14:06:58 +0200

    Signup initial structure and input reusable component

--- client/chat-client/src/pages/Signup.tsx
+++ client/chat-client/src/pages/Signup.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+//Components
+import { Input } from "../components/Input";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-8 text-center">Sign Up</h1>
+      <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+        <label htmlFor="">Username</label>
+        <Input />
+      </form>
+    </>
+  );
 };
 
 export default Signup;
