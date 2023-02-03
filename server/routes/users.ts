9bc3e82d1d09906a2e6904c86d428ec5d573ca8f
Nikita <lavro.nikita123@gmail.com>
2023-02-03 14:29:08 +0200

    Stream API keys

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,4 +1,10 @@
 import { errorCodes, FastifyInstance } from "fastify";
+import { StreamChat } from "stream-chat";
+
+const streamChat = StreamChat.getInstance(
+  process.env.STREAM_API_KEY!,
+  process.env.STREAM_PRIVATE_API_KEY!
+);
 
 export async function userRoutes(app: FastifyInstance) {
   app.post<{ Body: { id: string; name: string; image?: string } }>(
