77d4a22218a3de938e37fa9c1edd1e9ff37910f5
Nikita <lavro.nikita123@gmail.com>
2023-02-03 14:09:18 +0200

    set up CORS on the server

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,9 @@ config();
 
 //FASTIFY
 import fastify from "fastify";
+import cors from "@fastify/cors";
 
 const app = fastify();
+app.register(cors, { origin: process.env.CLIENT_URL });
 
 app.listen({ port: parseInt(process.env.PORT!) });
