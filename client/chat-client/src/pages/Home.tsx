fd99a6677b9194663b05580119ebb030de49c53e
Nikita <lavro.nikita123@gmail.com>
2023-02-06 20:26:59 +0200

    loadedChannel functionality and custom styles

--- client/chat-client/src/pages/Home.tsx
+++ client/chat-client/src/pages/Home.tsx
@@ -17,6 +17,12 @@ import {
 //Context
 import { useAuth, useLoggenInAuth } from "../context/AuthContext";
 
+//Components
+import { Button } from "../components/Button";
+
+//Router
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
   const { user, streamChat } = useLoggenInAuth();
 
@@ -41,9 +47,44 @@ const Home = () => {
 };
 
 function Channels({ loadedChannels }: ChannelListMessengerProps) {
+  const navigate = useNavigate();
+
   const { setActiveChannel, channel: activeChannel } = useChatContext();
 
-  return <div className="w-60 flex flex-col gap-4 m-3 h-full"></div>;
+  return (
+    <div className="w-60 flex flex-col gap-4 m-3 h-full">
+      <Button onClick={() => navigate("/channel/new")}>New Conversation</Button>
+      <hr className="border-gray-500" />
+      {loadedChannels != null && loadedChannels.length > 0
+        ? loadedChannels.map((channel) => {
+            const isActive = channel === activeChannel;
+            const extraClasses = isActive
+              ? "bg-blue-500 text-white"
+              : "hover:bg-blue-100 bg-gray-100";
+            return (
+              <button
+                onClick={() => {
+                  setActiveChannel(channel);
+                }}
+                disabled={isActive}
+                className={`p-4 rounded-lg flex gap-3 items-center ${extraClasses}`}
+                key={channel.id}
+              >
+                {channel.data?.image && (
+                  <img
+                    src={channel.data.image}
+                    className="w-10 h-10 rounded-full object-center object-cover"
+                  />
+                )}
+                <div className="text-ellipsis overflow-hidden whitespace-nowrap">
+                  {channel.data?.name || channel.id}
+                </div>
+              </button>
+            );
+          })
+        : "No Converstaions"}
+    </div>
+  );
 }
 
 export default Home;
