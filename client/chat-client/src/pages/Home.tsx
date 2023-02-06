463f5551e638f4ad358c60f945096413219efe6f
Nikita <lavro.nikita123@gmail.com>
2023-02-06 19:50:33 +0200

    rootLayout and Home component initial structure

--- client/chat-client/src/pages/Home.tsx
+++ client/chat-client/src/pages/Home.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
+//Stream-Chat
+import {
+  Chat,
+  LoadingIndicator,
+  ChannelList,
+  Channel,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+} from "stream-chat-react";
+
+//Context
+import { useAuth } from "../context/AuthContext";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { user, streamChat } = useAuth();
+
+  if (streamChat == null) return <LoadingIndicator></LoadingIndicator>;
+
+  return (
+    <Chat client={streamChat}>
+      <ChannelList />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+      </Channel>
+    </Chat>
+  );
 };
 
 export default Home;
--- client/chat-client/src/pages/layouts/RootLayout.tsx
+++ client/chat-client/src/pages/layouts/RootLayout.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+//Router
+import { Outlet, Navigate } from "react-router-dom";
+
+//Context
+import { useAuth } from "../../context/AuthContext";
 
 const RootLayout = () => {
+  const { user } = useAuth();
+
+  if (user == null) return <Navigate to="/login" />;
+
   return <Outlet></Outlet>;
 };
 
