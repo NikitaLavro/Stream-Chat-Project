fb9fd8906d7f59ea365eb384f17424e73514defd
Nikita <lavro.nikita123@gmail.com>
2023-02-09 00:06:29 +0200

    creating channel functionality

--- client/chat-client/src/pages/Channel.tsx
+++ client/chat-client/src/pages/Channel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, FormEvent } from "react";
 
 //Components
 import { FullScreenCard } from "../components/FullScreenCard";
@@ -7,42 +7,112 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 //React Query
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 //React Select
-import Select from "react-select";
+import Select, { SelectInstance } from "react-select";
+
+//Context
+import { useLoggenInAuth } from "../context/AuthContext";
+
+//Router
+import { useNavigate } from "react-router-dom";
 
 const Channel = () => {
+  const { streamChat, user } = useLoggenInAuth();
+  const navigate = useNavigate();
   const createChannel = useMutation({
-    mutationFn: () => Promise.resolve(),
-  });
+    mutationFn: ({
+      name,
+      memberIds,
+      imageUrl,
+    }: {
+      name: string;
+      memberIds: string[];
+      imageUrl?: string;
+    }) => {
+      if (streamChat == null) throw Error("Not connected");
 
+      return streamChat
+        .channel("messaging", crypto.randomUUID(), {
+          name,
+          image: imageUrl,
+          members: [user.id, ...memberIds],
+        })
+        .create();
+    },
+    onSuccess() {
+      navigate("/");
+    },
+  });
   const nameRef = useRef<HTMLInputElement>(null);
   const imageUrlRef = useRef<HTMLInputElement>(null);
+  const memberIdsRef =
+    useRef<SelectInstance<{ label: string; value: string }>>(null);
+
+  const users = useQuery({
+    queryKey: ["stream", "users"],
+    queryFn: () =>
+      streamChat!.queryUsers({ id: { $ne: user.id } }, { name: 1 }),
+    enabled: streamChat != null,
+  });
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const name = nameRef.current?.value;
+    const imageUrl = imageUrlRef.current?.value;
+    const selectOptions = memberIdsRef.current?.getValue();
+    if (
+      name == null ||
+      name === "" ||
+      selectOptions == null ||
+      selectOptions.length === 0
+    ) {
+      return;
+    }
+
+    createChannel.mutate({
+      name,
+      imageUrl,
+      memberIds: selectOptions.map((option) => option.value),
+    });
+  }
 
   return (
     <FullScreenCard>
       <FullScreenCard.Body>
-        <>
-          <h1 className="text-3xl font-bold mb-8 text-center">
-            New Converstaion
-          </h1>
-          <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
-            <label htmlFor="name">Name</label>
-            <Input id="name" required ref={nameRef} />
-            <label htmlFor="imageUrl">Name</label>
-            <Input id="imageUrl" required ref={imageUrlRef} />
-            <label htmlFor="members">Members</label>
-
-            <Button
-              disabled={createChannel.isLoading}
-              type="submit"
-              className="col-span-full"
-            >
-              {createChannel.isLoading ? "Loading.." : "Log In"}
-            </Button>
-          </form>
-        </>
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          New Conversation
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end"
+        >
+          <label htmlFor="name">Name</label>
+          <Input id="name" required ref={nameRef} />
+          <label htmlFor="imageUrl">Image Url</label>
+          <Input id="imageUrl" ref={imageUrlRef} />
+          <label htmlFor="members">Members</label>
+          <Select
+            ref={memberIdsRef}
+            id="members"
+            required
+            isMulti
+            classNames={{ container: () => "w-full" }}
+            isLoading={users.isLoading}
+            options={users.data?.users.map((user) => {
+              return { value: user.id, label: user.name || user.id };
+            })}
+          />
+          <Button
+            disabled={createChannel.isLoading}
+            type="submit"
+            className="col-span-full"
+          >
+            {createChannel.isLoading ? "Loading.." : "Create"}
+          </Button>
+        </form>
       </FullScreenCard.Body>
       <FullScreenCard.BelowCard>
         <Link to="/">Back</Link>
